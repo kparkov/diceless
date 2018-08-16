e6d502dade433e20936ef26b440e2318b1568720
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2018-08-16 08:43:54 +0200

    Fix unit tests

--- src/Library/Distribution.test.tsx
+++ src/Library/Distribution.test.tsx
@@ -3,15 +3,15 @@ import Distribution from "./Distribution";
 test('Should correctly assess 2d6', () => {
     const distribution = new Distribution([6, 6]);
     expect(distribution.combinationCounts()).toHaveLength(11);
-    expect(distribution.combinationCountsOf(7)).toEqual(6);
-    expect(distribution.combinationCountsOf(2)).toEqual(1);
+    expect(distribution.combinationCountsOf(7).combinations).toEqual(6);
+    expect(distribution.combinationCountsOf(2).combinations).toEqual(1);
 });
 
 test('Should correctly assess 2d6 + 1d8', () => {
     const distribution = new Distribution([6, 6, 8]);
     expect(distribution.combinationCounts()).toHaveLength(18);
-    expect(distribution.combinationCountsOf(11)).toEqual(32);
-    expect(distribution.combinationCountsOf(12)).toEqual(32);
-    expect(distribution.combinationCountsOf(20)).toEqual(1);
-    expect(distribution.percentage(distribution.combinationCountsOf(12).combinations)).toEqual(0.1111);
+    expect(distribution.combinationCountsOf(11).combinations).toEqual(32);
+    expect(distribution.combinationCountsOf(12).combinations).toEqual(32);
+    expect(distribution.combinationCountsOf(20).combinations).toEqual(1);
+    expect(distribution.percentage(distribution.combinationCountsOf(12).combinations)).toEqual(0.1111111111111111);
 })
